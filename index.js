744a4818d8401f08312a3880743bfa17e14a4cbf
F3L1P3D4RKofc <felipedark2509@gmail.com>
2022-11-01 23:59:13 -0300

    2° atualização do dia

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express'),
 app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/pages');
+app.use(express.static(__dirname + '/pages/static'));
+
 app.get('/', async(req, res) => res.send('ok'))
 
 app.listen(80, async() => console.log('site online'))
