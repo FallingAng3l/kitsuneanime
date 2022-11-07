4467a3d2f842e0c3a22f11ca3972ddff2684a8ff
F3L1P3D4RKofc <felipedark2509@gmail.com>
2022-11-07 00:07:35 -0300

    apaguei as krlhada e fui do ejs para o pug fds

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const session = require('express-session');
+
 require('dotenv').config();
+require('./strategies/discord')
 const express = require('express'),
     app = express(),
     mongoose = require('mongoose'),
@@ -6,43 +9,34 @@ const express = require('express'),
     Recaptcha = require('express-recaptcha').RecaptchaV2,
     recaptcha = new Recaptcha(process.env.siteKey, process.env.secretKey),
     passport = require('passport'),
-    discordStrategy = require('passport-discord').Strategy,
-    session = require('express-session');
+    discordStrategy = require('passport-discord').Strategy;
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.set('views', __dirname + '/pages');
-app.set('trust proxy', 1);
 app.use(express.static(__dirname + '/pages/static'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
-    secret: '_CeAuycgK36L9ge4KMLjgkvO9GCZnlv3',
+    secret: 'JHGSDGYUAKSJDB1235486543HGASD',
     resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 * 60 * 24 * 7}
 }));
-app.use(passport.session());
 app.use(passport.initialize());
+app.use(passport.session());
 
 for (let x of ['gets', 'posts']) require(`./handlers/${x}`)(app, recaptcha);
 
-passport.use(new discordStrategy({
-    clientID: '1037834650091012298',
-    clientSecret: '_CeAuycgK36L9ge4KMLjgkvO9GCZnlv3',
-    callbackURL: 'https://localhost/auth/discord/callback',
-    scope: ['identify', 'email']
-}, function (accessToken, refreshToken, profile, cb) {
-    return cb("", profile);
-}))
-
-app.get('/auth/discord', passport.authenticate('discord'))
-app.get('/auth/discord/callback', passport.authenticate('discord', {
-    failureRedirect: '/'
-}), function (req, res) {
-    res.send('ok') // Successful auth
+app.get('/discord', passport.authenticate('discord'), async(req, res) => {
+    res.sendStatus(200)
 });
+
+app.get('/discord/redirect', passport.authenticate('discord'), async(req, res) => {
+    res.send('sucesso!')
+});
+
 app.get('*', (req, res) => res.render('404'));
 
-app.listen(80, async () => console.log('site online'))
-mongoose.connect(process.env.mongoUrl, () => console.log('database online'))
+app.listen(80, async () => console.log('site online'));
+mongoose.connect(process.env.mongoUrl, () => console.log('database online'));
